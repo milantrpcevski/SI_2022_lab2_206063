872c01b68ae875c0e60241590b28cc6dbb4df68c
Milan Trpcevski <100216293+milantrpcevski@users.noreply.github.com>
2022-05-20 18:44:48 +0200

    Add files via upload

---
+++ SI_lab2_206063/src/test/java/SILab2Test.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    public List<String> kreirajLista(String ... elems){
+        return new ArrayList<>(Arrays.asList(elems));
+    }
+    @Test
+    public void everyStatement(){
+        IllegalArgumentException ex;
+        ex = assertThrows(IllegalArgumentException.class, ()->SILab2.function(kreirajLista()));
+        assertTrue(ex.getMessage().contains("List length should be greater than 0"));
+        ex = assertThrows(IllegalArgumentException.class, ()->SILab2.function(kreirajLista("0", "#", "0")));
+        assertTrue(ex.getMessage().contains("List length should be a perfect square"));
+        assertEquals(kreirajLista("2", "#", "#","#", "4", "#","#", "#", "#" ),
+                SILab2.function(kreirajLista("0", "#", "#","#", "0", "#","#", "#", "#" )));
+    }
+
+    @Test
+    public void everyBranch(){
+        IllegalArgumentException ex;
+        ex = assertThrows(IllegalArgumentException.class, ()->SILab2.function(kreirajLista()));
+        assertTrue(ex.getMessage().contains("List length should be greater than 0"));
+        ex = assertThrows(IllegalArgumentException.class, ()->SILab2.function(kreirajLista("0", "#", "0")));
+        assertTrue(ex.getMessage().contains("List length should be a perfect square"));
+        assertEquals(kreirajLista("#", "#", "#","2", "#", "#","4", "#", "#", "#", "#","#","#", "#","2","1"),
+                SILab2.function(kreirajLista("#", "#", "#","0", "#", "#","0", "#", "#", "#", "#","#","#", "#","0","0")));
+    }
+}
